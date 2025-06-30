19f06b4ef562905d971972a5dc1cae8a04f65258
arul-stack <23124011@srcas.ac.in>
2025-06-30 09:27:58 +0530

    Pushed
    Pushed
    commands

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,32 @@
+import '../css/Favorites.css'
+import { useMovieContext } from '../contexts/MovieContext';
+import MovieCard from '../Components/MovieCard';
+
+
+function Favorites() {
+
+    const {favorites} = useMovieContext();
+
+    if(favorites) {
+        return (
+            <div className='favorites'>
+                <h2> Your favorites</h2>
+                <div className='movies-grid'>
+                    {favorites.map((movie)=> (
+                        <MovieCard movie={movie} key={movie.id}/>
+                        )
+                    )}
+
+                </div>
+
+            </div>
+        )
+    }
+
+    return <div className="favorites-empty">
+        <h2>No favorite movies yet</h2>
+        <p>start adding movies to your favorites</p>
+    </div>
+}
+
+export default Favorites;
